4d415b8ed3411d65cdab5d43238d7db8d9182504
Saeed Mohamed <132412767+Saeed096@users.noreply.github.com>
2024-05-31 09:49:54 +0300

    .

--- obj/Debug/net6.0/HotelBookingSystem.AssemblyInfo.cs
+++ obj/Debug/net6.0/HotelBookingSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelBookingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2b7c8e42bf93867005b686a9cf53fa32b92af4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelBookingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelBookingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
